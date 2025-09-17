fa2783dcc51952e0d79520c93a23d23dab825578
andrewmiller-2 <andrew.miller2@student.fairfield.edu>
2025-09-17 18:39:02 -0400

    v.05

--- assignment-1-test-v.01.py
+++ assignment-1-test-v.01.py
@@ -1,1 +1,19 @@
-# Prompt the user to do anything
+# Prompt the user for revenue (float) and cost (float)
+
+revenue = float(input("Enter revenue: "))
+cost = float(input("Enter cost: "))
+profit = revenue - cost
+
+# Calculate profit = revenue - cost
+
+#calulate margin = profit / revenue
+# Print the results fomatted nicely, e.g. "Profit: $X.XX | Margin: Y.YY%" round to 2 decimal places
+
+if revenue > 0: 
+    margin = ( profit / revenue ) * 100
+    profit = revenue - cost
+
+    print(f"Profit: ${profit:.2f} | Margin: {margin:.2f}%")
+else:
+    print("Revenue must be greater than 0 to calculate margin.")
+
